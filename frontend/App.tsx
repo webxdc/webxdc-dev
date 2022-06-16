bc48dd455da1bd0506a4bcf2488fdc7f4412532e
Martijn Faassen <faassen@startifact.com>
2022-06-16 14:57:45 +0200

    No more Comp

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,11 +1,9 @@
 import type { Component } from "solid-js";
-import Comp from "./Comp";
 
 const App: Component = () => {
   return (
     <>
       <h1>Webxdc-dev</h1>
-      <Comp />
     </>
   );
 };

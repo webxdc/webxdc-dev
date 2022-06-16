d65b69a6aa0ff269877149060efd68debd36b7a5
Martijn Faassen <faassen@startifact.com>
2022-06-16 16:34:51 +0200

    Allow opening of links to instances

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,9 +1,36 @@
 import type { Component } from "solid-js";
+import { For } from "solid-js";
+import { createStore, produce } from "solid-js/store";
+
+type InstanceData = {
+  id: number;
+  url: string;
+};
+
+const [instances, setInstances] = createStore<InstanceData[]>([
+  { id: 3001, url: "http://localhost:3001" },
+  { id: 3002, url: "http://localhost:3002" },
+]);
+
+const Instance: Component<{ instance: InstanceData }> = (props) => {
+  return (
+    <li>
+      <a target="_blank" href={props.instance.url}>
+        {props.instance.id}
+      </a>
+    </li>
+  );
+};
 
 const App: Component = () => {
   return (
     <>
       <h1>Webxdc-dev</h1>
+      <ul>
+        <For each={instances}>
+          {(instance) => <Instance instance={instance} />}
+        </For>
+      </ul>
     </>
   );
 };

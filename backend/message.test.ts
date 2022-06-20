09627f48c042dc138f56e0194bf8d8a6bd0bddc9
Martijn Faassen <faassen@startifact.com>
2022-06-20 15:13:09 +0200

    Partial catch up also works

--- backend/message.test.ts
+++ backend/message.test.ts
@@ -94,3 +94,36 @@ test("other starts listening later", () => {
   expect(client0Heard).toMatchObject([{ payload: "Hello", serial: 1 }]);
   expect(client1Heard).toMatchObject([{ payload: "Hello", serial: 1 }]);
 });
+
+test("other starts listening later but is partially caught up", () => {
+  const processor = createProcessor<string>();
+  const client0 = processor.createClient("3001");
+  const client1 = processor.createClient("3002");
+
+  const client0Heard: ReceivedUpdate<string>[] = [];
+  const client1Heard: ReceivedUpdate<string>[] = [];
+
+  client0.setUpdateListener((update) => {
+    client0Heard.push(update);
+  }, 0);
+
+  client0.sendUpdate({ payload: "Hello" }, "update");
+  client1.sendUpdate({ payload: "Bye" }, "update 2");
+  expect(client0Heard).toMatchObject([
+    { payload: "Hello", serial: 1 },
+    { payload: "Bye", serial: 2 },
+  ]);
+  // we only join later, so we haven't heard a thing yet
+  expect(client1Heard).toMatchObject([]);
+
+  // start at 1, as we're already partially caught up
+  client1.setUpdateListener((update) => {
+    client1Heard.push(update);
+  }, 1);
+
+  expect(client0Heard).toMatchObject([
+    { payload: "Hello", serial: 1 },
+    { payload: "Bye", serial: 2 },
+  ]);
+  expect(client1Heard).toMatchObject([{ payload: "Bye", serial: 2 }]);
+});

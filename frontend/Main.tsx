e3e4e7da1abb63b0e3b2f7d6a3de211c57932040
Martijn Faassen <faassen@startifact.com>
2022-06-30 16:02:11 +0200

    Pull out MessageDetails

--- frontend/Main.tsx
+++ frontend/Main.tsx
@@ -37,55 +37,11 @@ import Filter from "./Filter";
 import { instances, InstanceData, getMessages } from "./store";
 import InstancesButtons from "./InstancesButtons";
 import { Message } from "../types/message";
-import RecordRow from "./RecordRow";
+import MessageDetails from "./MessageDetails";
 import { instanceIdEntries } from "./instanceIdEntries";
-import { sent, received, isUpdateMessage } from "./store";
+import { sent, received } from "./store";
 import MessageRow from "./MessageRow";
 import { scrollToLastMessage, scrollToDevice } from "./scroll";
-
-const MessageDetails: Component<{ message: Message }> = (props) => {
-  return (
-    <Table dense>
-      <Tbody>
-        <RecordRow label="instance id">
-          <Text as="span" color={props.message.instanceColor}>
-            {props.message.instanceId}
-          </Text>
-        </RecordRow>
-        <RecordRow label="type">{props.message.type}</RecordRow>
-        <Show when={isUpdateMessage(props.message) && props.message}>
-          {(message) => {
-            return (
-              <>
-                <RecordRow label="descr">{message.descr}</RecordRow>
-                <RecordRow label="serial">{message.update.serial}</RecordRow>
-                <Show when={message.type === "received"}>
-                  <RecordRow label="max serial">
-                    {message.update.max_serial}
-                  </RecordRow>
-                </Show>
-                <RecordRow label="info">{message.update.info}</RecordRow>
-                <RecordRow label="document">
-                  {message.update.document}
-                </RecordRow>
-                <RecordRow label="summary">{message.update.summary}</RecordRow>
-                <RecordRow label="payload">
-                  <Box maxHeight="9rem" overflow="auto">
-                    <pre>
-                      <code>
-                        {JSON.stringify(message.update.payload, null, 2)}
-                      </code>
-                    </pre>
-                  </Box>
-                </RecordRow>
-              </>
-            );
-          }}
-        </Show>
-      </Tbody>
-    </Table>
-  );
-};
 
 type Search = {
   instanceId?: string;

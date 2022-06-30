d3a5c30a9affc926fd161425610a31169d03f197
Martijn Faassen <faassen@startifact.com>
2022-06-30 15:36:11 +0200

    Make mobile now the main tab

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -3,8 +3,6 @@ import { JSX, createSignal, createEffect } from "solid-js";
 import { Box, Tabs, TabList, Tab, TabPanel } from "@hope-ui/solid";
 import { Route, Routes, useNavigate, useLocation } from "solid-app-router";
 
-import Instances from "./Instances";
-import Messages from "./Messages";
 import Info from "./Info";
 import Mobile from "./Mobile";
 
@@ -19,10 +17,8 @@ const Panel: Component<{ children: JSX.Element }> = (props) => {
 const AppRoutes: Component = () => {
   return (
     <Routes>
-      <Route path="/" element={<Instances />} />
+      <Route path="/" element={<Mobile />} />
       <Route path="/info" element={<Info />} />
-      <Route path="/messages" element={<Messages />} />
-      <Route path="/mobile" element={<Mobile />} />
     </Routes>
   );
 };
@@ -37,10 +33,6 @@ const App: Component = () => {
       setTabIndex(0);
     } else if (location.pathname === "/info") {
       setTabIndex(1);
-    } else if (location.pathname === "/messages") {
-      setTabIndex(2);
-    } else if (location.pathname === "/mobile") {
-      setTabIndex(3);
     }
   });
 
@@ -51,10 +43,6 @@ const App: Component = () => {
       navigate("/");
     } else if (index === 1) {
       navigate("/info");
-    } else if (index === 2) {
-      navigate("/messages");
-    } else if (index === 3) {
-      navigate("/mobile");
     }
     setTabIndex(index);
   };
@@ -62,21 +50,9 @@ const App: Component = () => {
   return (
     <Tabs index={tabIndex()} onChange={handleTabsChange}>
       <TabList>
-        <Tab>Instances</Tab>
+        <Tab>Main</Tab>
         <Tab>Info</Tab>
-        <Tab>Messages</Tab>
-        <Tab>Mobile</Tab>
       </TabList>
-      <TabPanel>
-        <Panel>
-          <AppRoutes />
-        </Panel>
-      </TabPanel>
-      <TabPanel>
-        <Panel>
-          <AppRoutes />
-        </Panel>
-      </TabPanel>
       <TabPanel>
         <Panel>
           <AppRoutes />

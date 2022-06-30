2a824557851c53035349a28999d898f4984567a1
Martijn Faassen <faassen@startifact.com>
2022-06-30 16:31:42 +0200

    Factor out InstanceHeader

--- frontend/Instance.tsx
+++ frontend/Instance.tsx
@@ -1,4 +1,4 @@
-import { Component, Show, createMemo, JSX } from "solid-js";
+import { Component, Show, createMemo, JSX, Accessor } from "solid-js";
 import {
   Flex,
   Text,
@@ -19,14 +19,6 @@ const Instance: Component<{
   instance: InstanceData;
   setSearch: (search: Search) => void;
 }> = (props) => {
-  const sentCount = createMemo(() => {
-    return sent(props.instance.id);
-  });
-
-  const receivedCount = createMemo(() => {
-    return received(props.instance.id);
-  });
-
   let iframe_ref: HTMLIFrameElement | undefined = undefined;
 
   const handleReload = () => {
@@ -37,82 +29,20 @@ const Instance: Component<{
     iframe_ref.contentWindow?.postMessage("reload", props.instance.url);
   };
 
-  const handleOpenInTab = () => {
-    window.open(props.instance.url, "_blank");
-  };
-
   const { isOpen, onOpen, onClose } = createDisclosure({
     defaultIsOpen: false,
   });
 
   return (
-    <Flex flexDirection="column">
-      <Flex
-        id={"instance-" + props.instance.id}
-        gap="$1"
-        justifyContent="space-between"
-        alignItems="center"
-      >
-        <Tooltip label="Click to see all messages for this instance">
-          <Text
-            color={props.instance.color}
-            fontSize="$2xl"
-            fontWeight="bold"
-            onClick={() => props.setSearch({ instanceId: props.instance.id })}
-          >
-            {props.instance.id}
-          </Text>
-        </Tooltip>
-        <Tooltip label="Click to see all sent messages for this instance">
-          <Badge
-            onClick={() =>
-              props.setSearch({ instanceId: props.instance.id, type: "sent" })
-            }
-          >
-            Sent: {sentCount}
-          </Badge>
-        </Tooltip>
-        <Tooltip label="Click to see all received messages for this instance">
-          <Badge
-            onClick={() =>
-              props.setSearch({
-                instanceId: props.instance.id,
-                type: "received",
-              })
-            }
-          >
-            Received: {receivedCount}
-          </Badge>
-        </Tooltip>
-        <Flex gap="$1">
-          <InstanceButton
-            label={`Open new browser tab for instance ${props.instance.id}`}
-            onClick={handleOpenInTab}
-            icon={<FiExternalLink size={22} color="#000000" />}
-          />
-          <Show
-            when={isOpen()}
-            fallback={
-              <InstanceButton
-                label="Start"
-                onClick={onOpen}
-                icon={<IoPlay size={22} color="#000000" />}
-              />
-            }
-          >
-            <InstanceButton
-              label="Stop"
-              onClick={onClose}
-              icon={<IoStop size={22} color="#000000" />}
-            />
-          </Show>
-          <InstanceButton
-            label="Reload"
-            onClick={handleReload}
-            icon={<IoRefreshOutline size={22} color="#000000" />}
-          />
-        </Flex>
-      </Flex>
+    <Flex id={"instance-" + props.instance.id} flexDirection="column">
+      <InstanceHeader
+        instance={props.instance}
+        setSearch={props.setSearch}
+        onReload={handleReload}
+        onStart={onOpen}
+        onStop={onClose}
+        isStarted={isOpen}
+      />
       <Show
         when={isOpen()}
         fallback={<FrameStopped instance={props.instance} onStart={onOpen} />}
@@ -129,6 +59,91 @@ const Instance: Component<{
           }}
         ></iframe>
       </Show>
+    </Flex>
+  );
+};
+
+const InstanceHeader: Component<{
+  instance: InstanceData;
+  setSearch: (search: Search) => void;
+  onReload: () => void;
+  onStart: () => void;
+  onStop: () => void;
+  isStarted: Accessor<boolean>;
+}> = (props) => {
+  const sentCount = createMemo(() => {
+    return sent(props.instance.id);
+  });
+
+  const receivedCount = createMemo(() => {
+    return received(props.instance.id);
+  });
+
+  const handleOpenInTab = () => {
+    window.open(props.instance.url, "_blank");
+  };
+
+  return (
+    <Flex gap="$1" justifyContent="space-between" alignItems="center">
+      <Tooltip label="Click to see all messages for this instance">
+        <Text
+          color={props.instance.color}
+          fontSize="$2xl"
+          fontWeight="bold"
+          onClick={() => props.setSearch({ instanceId: props.instance.id })}
+        >
+          {props.instance.id}
+        </Text>
+      </Tooltip>
+      <Tooltip label="Click to see all sent messages for this instance">
+        <Badge
+          onClick={() =>
+            props.setSearch({ instanceId: props.instance.id, type: "sent" })
+          }
+        >
+          Sent: {sentCount}
+        </Badge>
+      </Tooltip>
+      <Tooltip label="Click to see all received messages for this instance">
+        <Badge
+          onClick={() =>
+            props.setSearch({
+              instanceId: props.instance.id,
+              type: "received",
+            })
+          }
+        >
+          Received: {receivedCount}
+        </Badge>
+      </Tooltip>
+      <Flex gap="$1">
+        <InstanceButton
+          label={`Open new browser tab for instance ${props.instance.id}`}
+          onClick={handleOpenInTab}
+          icon={<FiExternalLink size={22} color="#000000" />}
+        />
+        <Show
+          when={props.isStarted()}
+          fallback={
+            <InstanceButton
+              label="Start"
+              onClick={props.onStart}
+              icon={<IoPlay size={22} color="#000000" />}
+            />
+          }
+        >
+          <InstanceButton
+            label="Stop"
+            onClick={props.onStop}
+            icon={<IoStop size={22} color="#000000" />}
+          />
+        </Show>
+        <InstanceButton
+          label="Reload"
+          onClick={props.onReload}
+          icon={<IoRefreshOutline size={22} color="#000000" />}
+        />
+      </Flex>
     </Flex>
   );
 };

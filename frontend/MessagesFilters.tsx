40e29be5e0e6bbe78abdda639c00a45fe3e4577d
Martijn Faassen <faassen@startifact.com>
2022-06-30 15:51:56 +0200

    Clean up unused code

--- frontend/MessagesFilters.tsx
+++ frontend/MessagesFilters.tsx
@@ -1,12 +1,6 @@
-import { Component, createMemo } from "solid-js";
-import { useSearchParams } from "solid-app-router";
-import { Flex } from "@hope-ui/solid";
+import { createMemo } from "solid-js";
 
-import Filter from "./Filter";
 import { instances } from "./store";
-
-type ParamsType = ReturnType<typeof useSearchParams>[0];
-type SetParamsType = ReturnType<typeof useSearchParams>[1];
 
 export const instanceIdEntries = createMemo(() => {
   const resolvedInstances = instances();
@@ -25,63 +19,3 @@ export const instanceIdEntries = createMemo(() => {
     })),
   ];
 });
-
-const Filters: Component<{
-  searchParams: ParamsType;
-  setSearchParams: SetParamsType;
-}> = (props) => {
-  return (
-    <Flex justifyContent="flex-start" gap="$5">
-      <Filter
-        label="instanceId"
-        entries={instanceIdEntries()}
-        value={props.searchParams.instanceId || "*"}
-        onChange={(value) => {
-          if (value === "*") {
-            props.setSearchParams(
-              {
-                ...props.searchParams,
-                instanceId: undefined,
-              },
-              { replace: true }
-            );
-          } else {
-            props.setSearchParams(
-              {
-                ...props.searchParams,
-                instanceId: value,
-              },
-              { replace: true }
-            );
-          }
-        }}
-      />
-      <Filter
-        label="type"
-        entries={[
-          { value: "*", text: "All types" },
-          { value: "sent", text: "Sent" },
-          { value: "received", text: "Received" },
-          { value: "connect", text: "Connect" },
-          { value: "clear", text: "Clear" },
-        ]}
-        value={props.searchParams.type || "*"}
-        onChange={(value) => {
-          if (value === "*") {
-            props.setSearchParams(
-              { ...props.searchParams, type: undefined },
-              { replace: true }
-            );
-          } else {
-            props.setSearchParams(
-              { ...props.searchParams, type: value },
-              { replace: true }
-            );
-          }
-        }}
-      />
-    </Flex>
-  );
-};
-
-export default Filters;

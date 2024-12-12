78c56775c53e87786f1f2546adf45e2f899b9cb5
Sebastian Klähn <scoreplayer2000@gmail.com>
2024-12-12 18:17:18 +0100

    remove unused packages

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
     {
       devShells = forEachSupportedSystem ({ pkgs }: {
         default = pkgs.mkShell {
-          packages = with pkgs; [ node2nix nodejs nodePackages.pnpm nodePackages.typescript-language-server ];
+          packages = with pkgs; [ nodejs nodePackages.typescript-language-server ];
         };
       });
     };

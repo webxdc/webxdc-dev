a5f23978ec165e4ffd26dc608bafc5dddd4f044c
lms <lms@linkping.org>
2025-02-28 02:35:32 +0100

    Use async action

--- backend/run.ts
+++ backend/run.ts
@@ -12,7 +12,7 @@ export type Inject = {
   getIndexHtml: () => string;
 };
 
-function actualRun(appInfo: AppInfo, options: Options, inject: Inject): void {
+async function actualRun(appInfo: AppInfo, options: Options, inject: Inject): Promise<void> {
   const { injectFrontend, injectSim, getIndexHtml } = inject;
   const instances = new Instances(appInfo, injectSim, options);
 
@@ -37,7 +37,7 @@ function actualRun(appInfo: AppInfo, options: Options, inject: Inject): void {
   open("http://localhost:" + options.basePort);
 }
 
-export function run(locationStr: string, options: Options, inject: Inject) {
+export async function run(locationStr: string, options: Options, inject: Inject): Promise<void> {
   let location: Location;
   try {
     location = getLocation(locationStr);
@@ -57,15 +57,14 @@ export function run(locationStr: string, options: Options, inject: Inject) {
 
   console.log("Starting webxdc project in:", locationStr);
 
-  getAppInfo(location)
-    .then((appInfo) => {
-      actualRun(appInfo, options, inject);
-    })
-    .catch((e) => {
-      if (e instanceof AppInfoError) {
-        console.error(e.message);
-        return;
-      }
-      throw e;
-    });
+  try {
+    const appInfo = await getAppInfo(location)
+    await actualRun(appInfo, options, inject);
+  } catch (e) {
+    if (e instanceof AppInfoError) {
+      console.error(e.message);
+      return;
+    }
+    throw e;
+  }
 }

a51d1743ea777fbefa79ac70ca0dfe9ab2e52efb
Simon Laux <mobile.info@simonlaux.de>
2025-11-23 15:14:10 +0100

    deprecate old webxdc-dev npm package

---
+++ move.js
@@ -0,0 +1,16 @@
+const newName = "@webxdc/webxdc-dev"
+
+console.log(`
+!!! Error !!!:
+You downloaded a deprecated version of webxdc-dev.
+The package was renamed to ${newName}.
+
+Install the new package globally:
+npm i -g ${newName}
+
+Install the new package just in your project:
+npm i ${newName}
+
+Install it temporarily and run it directly:
+npx ${newName}
+`)
